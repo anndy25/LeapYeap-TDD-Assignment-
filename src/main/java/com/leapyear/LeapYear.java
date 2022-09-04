29bdec14af692bca281c89392872839283c1eabe
Aniket Satish Mane <aniketsmd310@gmail.com>
2022-09-04 13:25:02 +0530

    write a code to pass 1st test case

--- src/main/java/com/leapyear/LeapYear.java
+++ src/main/java/com/leapyear/LeapYear.java
@@ -4,7 +4,11 @@ public class LeapYear {
     
     boolean isLeapYear(int year){
         if(year < 0 ) throw new IllegalArgumentException("Year can not be negative");
-       return false;
+        if (year % 4 == 0) {
+                return Boolean.TRUE;// A Leap Year
+        } else
+            return Boolean.FALSE;// Not a Leap Year;
+       
     }
  }
  
